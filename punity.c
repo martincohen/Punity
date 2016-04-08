fd238d4a3f7e253c8bb8011f5cfa908ca817d31b
martincohen <m@coh.io>
2016-04-08 21:32:07 +0200

    Added licenses to files.

--- punity.c
+++ punity.c
@@ -1,3 +1,10 @@
+/** 
+ * Copyright (c) 2016 martincohen
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */ 
+
 #include "punity.h"
 
 #if PLATFORM_OSX || PLATFORM_LINUX
--- punity.h
+++ punity.h
@@ -1,3 +1,10 @@
+/** 
+ * Copyright (c) 2016 martincohen
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */ 
+
 #ifndef PUNITY_H
 #define PUNITY_H
 
